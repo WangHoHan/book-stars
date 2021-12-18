9f09435bdf84d92c4cf55ce94ed82660e348aa65
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2021-12-18 14:52:01 +0100

    basic model

---
+++ bookstars.py
@@ -0,0 +1,39 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from vowpalwabbit import pyvw
+import csv
+import os
+import pickle
+
+model = None
+if os.path.isfile("model.vwmodel"):
+    model = pyvw.vw('--quiet -i model.vwmodel')
+else:
+    data = []
+    if os.path.isfile("bookstars.pkl"):
+        book_stars_pkl = open("bookstars.pkl", "rb")
+        data = pickle.load(book_stars_pkl)
+        book_stars_pkl.close()
+    else:
+        with open("book_stars.csv", newline='', encoding='utf-8') as book_stars:
+            reader = csv.reader(book_stars, delimiter=',', quotechar='"')
+            for star, opinion in reader:
+                data.append(star + " |" + opinion)
+            book_stars_pkl = open("bookstars.pkl", "wb")
+            pickle.dump(data, book_stars_pkl)
+            book_stars_pkl.close()
+    train_data, test_data = train_test_split(data, train_size=0.4, test_size=0.1, shuffle=True)
+
+    model = pyvw.vw('--quiet -c -f model.vwmodel')
+    for elem in train_data:
+        model.learn(elem)
+    stars, predicts = [], []
+    for elem in test_data:
+        stars.append(int(elem.split(" | ")[0]))
+        predicts.append(model.predict("| " + elem.split(" | ")[1]))
+    model.finish()
+    print("rmse: " + str(mean_squared_error(stars, predicts, squared=False)))
+print("wpisz opinię o książce:")
+opinion = input()
+book_star = model.predict("| " + opinion)
+print("ilość gwiazdek: " + str(round((book_star / 2), 2)) + '/5')
--- scraper.py
+++ scraper.py
@@ -13,13 +13,13 @@ if no_pages_match:
 with open("book_stars.csv", "w", newline='', encoding='utf-8') as book_stars:
     writer = csv.writer(book_stars)
     for page in range(1, no_pages + 1):
-        print("page: ", page, "/", no_pages, sep="")
+        # print("page: ", page, "/", no_pages, sep="")
         lubimyczytac = f"https://lubimyczytac.pl/katalog?page={page}&listId=booksFilteredList&onlyPublished=1&publishedYear%5B0%5D=1200&publishedYear%5B1%5D=2021&catalogSortBy=published-desc&paginatorType=Standard"
         page = requests.get(lubimyczytac).text
         doc = BeautifulSoup(page, "html.parser")
         books_match = re.findall(r'href=\"(.+)\">', str(doc.findAll(class_="authorAllBooks__singleTextTitle float-left")))
         for book in books_match:
-            print("book: ", book.rsplit('/', 1)[-1], sep="")
+            # print("book: ", book.rsplit('/', 1)[-1], sep="")
             lubimyczytac = f"https://lubimyczytac.pl" + book
             page = requests.get(lubimyczytac).text
             doc = BeautifulSoup(page, "html.parser")
@@ -31,8 +31,8 @@ with open("book_stars.csv", "w", newline='', encoding='utf-8') as book_stars:
                     if opinion:
                         opinion[0] = opinion[0].replace("\n", " ").replace("<p>", "").replace("</p>", "").replace("<br>", "").replace("</br>", "").replace("<br/>", "")
                         opinion[0] = re.sub(" +", " ", opinion[0])
-                        print("star: ", star.group(1), sep="", end=", ")
-                        print("opinion: ", opinion[0], sep="")
+                        # print("star: ", star.group(1), sep="", end=", ")
+                        # print("opinion: ", opinion[0], sep="")
                         try:
                             writer.writerow([star.group(1), opinion[0]])
                         except:
