d0e779257ad4e62de82ea2a2a2a1e97d64678d6d
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2021-12-21 17:07:28 +0100

    add tokenization, lemmatization, stop words, words lowering

--- bookstars.py
+++ bookstars.py
@@ -1,33 +1,41 @@
 from vowpalwabbit import pyvw
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+import spacy
 import csv
 import os
-import pickle
+import re
+
+
+def get_opinion_params(_opinion):
+    _opinion = _opinion.lower()
+    doc = nlp(_opinion)
+    tokens = []
+    for token in doc:
+        tokens.append(token.lemma_)
+    tokens = [token for token in tokens if not re.match(r'\W+', token)]
+    tokens = list(dict.fromkeys(tokens))
+    tokens = [token for token in tokens if token not in stop_words]
+    return tokens
+
 
 model = None
+nlp = spacy.load("pl_core_news_lg")
+star_opinion_datasets = ["book_stars.csv", "book_stars1.csv"]
+stop_words_txt = open("polish_stopwords.txt", "r", encoding='utf-8')
+stop_words = [line.rstrip() for line in stop_words_txt]
+stop_words_txt.close()
 if os.path.isfile("model.vwmodel"):
     model = pyvw.vw('--quiet -i model.vwmodel')
 else:
     data = []
-    if os.path.isfile("bookstars.pkl"):
-        book_stars_pkl = open("bookstars.pkl", "rb")
-        data = pickle.load(book_stars_pkl)
-        book_stars_pkl.close()
-    else:
-        with open("book_stars.csv", newline='', encoding='utf-8') as book_stars:
+    for dataset in star_opinion_datasets:
+        with open(dataset, newline='', encoding='utf-8') as book_stars:
             reader = csv.reader(book_stars, delimiter=',', quotechar='"')
             for star, opinion in reader:
-                data.append(star + " |" + opinion)
-        with open("book_stars1.csv", newline='', encoding='utf-8') as book_stars:
-            reader = csv.reader(book_stars, delimiter=',', quotechar='"')
-            for star, opinion in reader:
-                data.append(star + " |" + opinion)
-    book_stars_pkl = open("bookstars.pkl", "wb")
-    pickle.dump(data, book_stars_pkl)
-    book_stars_pkl.close()
+                params = get_opinion_params(opinion)
+                data.append(star + " | " + " ".join(params))
     train_data, test_data = train_test_split(data, train_size=0.9, test_size=0.1, shuffle=True)
-
     model = pyvw.vw('--quiet -c -f model.vwmodel')
     for elem in train_data:
         model.learn(elem)
@@ -37,7 +45,9 @@ else:
         predicts.append(model.predict("| " + elem.split(" | ")[1]))
     model.finish()
     print("rmse: " + str(mean_squared_error(stars, predicts, squared=False)))
-print("wpisz opinię o książce:")
+print("wpisz opinię o książce: ")
 opinion = input()
-book_star = model.predict("| " + opinion)
+print("")
+params = get_opinion_params(opinion)
+book_star = model.predict("| " + " ".join(params))
 print("ilość gwiazdek: " + str(round((book_star / 2), 2)) + '/5')
